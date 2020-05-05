1272fd55418dd21f54a7697e7a30e7213d417419
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-05-05 01:02:34 -0400

    getting closer to achieving identical functionality

--- Alabama.py
+++ Alabama.py
@@ -2,16 +2,16 @@ import random
 import time
 from builtins import print
 import requests
+import datetime
 
 
 from bs4 import BeautifulSoup
 
 import SetProxy
 
-# dateTest = "06/02/01"
-# dateFormatted = datetime.datetime.strptime(dateTest, '%m/%d/%y')
-# print(dateFormatted.date().strftime("%m-%d-%Y"))
-nextBiz = 19996
+dateTest = "06/02/01"
+
+nextBiz = 268908
 
 baseURL = "http://arc-sos.state.al.us/cgi/corpdetail.mbr/detail?page=number&num1="
 totalScraped = 0
@@ -28,7 +28,9 @@ def parse():
     # will need to catch or identify indexError
     f = open("Alabama.csv", "a")
 
+
     global nextBiz, totalScraped, count
+    nextBiz += 1
     totalScraped += 1
     count += 1
     if totalScraped >= 65:
@@ -78,16 +80,59 @@ def parse():
     print(desc[1].text)
     print(value[1].text)
 
-    f.write(url + ", US, AL, " + '"' + name[0].text + '"' + ",\n")
-    f.close()
+    f.write(url + ", US, AL, " + '"' + name[0].text + '"' + ",") #name and constant variables
+    f.write(value[0].text + ',') #entity number
+    f.write(value[1].text + ",")
+
+    # get the core business type
+    if "Limited" in value[1].text:
+        f.write("LLC,")
+    elif "Corporation" in value[1].text:
+        f.write("Corporation,")
+    else:
+        f.write("---,")
+
+    f.write(value[4].text + ",") #status
+
+    if value[4].text == "Dissolved":
+        dateFormatted = datetime.datetime.strptime(value[5].text, '%m-%d-%Y')
+        f.write(dateFormatted.date().strftime("%m/%d/%Y") + ",") # dissolve date
+        dateFormatted = datetime.datetime.strptime(value[7].text.replace(" ", ""), '%m-%d-%Y')
+        f.write(dateFormatted.date().strftime("%m/%d/%Y") + ",")  #formation date
+        f.write('"' + value[8].text + '",') #registerered agent name
+        f.write('"' + value[9].text + '",')  # registerered office add.
+        f.write('"' + value[10].text + '",\n')  # registerered mailing add.
+        f.close()
+        totalScraped += 1
+        #print(totalScraped) add a global counter
+        parse()
+    elif value[4].text == "Withdrawn":
+        dateFormatted = datetime.datetime.strptime(value[5].text, '%m-%d-%Y')
+        f.write(dateFormatted.date().strftime("%m/%d/%Y") + ",")  # dissolve date
+        dateFormatted = datetime.datetime.strptime(value[8].text.replace(" ", ""), '%m-%d-%Y')
+        f.write(dateFormatted.date().strftime("%m/%d/%Y") + ",")  # formation date
+        f.write('"' + value[10].text + '",') #reg agent name
+        f.write('"' + value[11].text + '",') #reg address
+        f.write('"' + value[12].text + '",\n') #reg mailing
+        f.close()
+        totalScraped += 1
+        parse()
+    elif value[4].text == "Exists":
+        f.write("\n")
+
+
+
+
+
+
 
     end = time.time() / 60
     time_elapsed = (end - start)
     request_per_minute = count / time_elapsed
     print("average request per minute", end=" ")
     print(round(request_per_minute, 2))
-    nextBiz += 1
 
+    f.close()
     parse()
 
 
